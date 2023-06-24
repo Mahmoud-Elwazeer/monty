76789d4a8b2bbf5182774ed694b9f6b624448e51
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-24 07:39:09 -0700

    test

--- main.c
+++ main.c
@@ -8,11 +8,20 @@
  */
 int main(int ac, char *av[])
 {
+	FILE *file;
+
 	if (ac != 1)
 	{
 		printf("USAGE: monty %s", av[1]);
 		exit(EXIT_FAILURE);
 	}
+	file = fopen(av[1], "r");
+	if (file == NULL)
+	{
+		printf("Error: Can't open file %s", av[1]);
+		exit(EXIT_FAILURE);
+	}
 
+	fclose(file);
 	return (0);
 }
