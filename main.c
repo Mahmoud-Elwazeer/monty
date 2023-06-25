9701844e86d077ace30aff19e52ae75304bae04e
Mahmoud Elwazeer <elwazeermahmoud48@gmail.com>
2023-06-25 04:54:13 -0700

    test

--- main.c
+++ main.c
@@ -9,18 +9,27 @@
 int main(int ac, char *av[])
 {
 	FILE *file;
+	char *line = NULL;
+	size_t size = 0;
+	ssize_t nread;
 
-	if (ac != 1)
+	if (ac != 2)
 	{
-		printf("USAGE: monty %s", av[1]);
+		printf("USAGE: monty %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 	file = fopen(av[1], "r");
 	if (file == NULL)
 	{
-		printf("Error: Can't open file %s", av[1]);
+		printf("Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
+	while ((nread = getline(&line, &size, file)) != -1)
+	{
+		printf("%s", line);
+	}
+	if (line)
+		free(line);
 
 	fclose(file);
 	return (0);
