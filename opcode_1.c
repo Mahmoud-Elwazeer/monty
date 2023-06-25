2d0078180d47c4049d279f0f27b3bdef0d93f8a7
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-25 12:39:18 -0700

    update

--- opcode_1.c
+++ opcode_1.c
@@ -73,8 +73,9 @@ void pint(stack_t **stack, unsigned int line_number)
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
 	stack_t *temp = *stack;
+	(void)line_number;
+
 
 	if (stack == NULL)
 		return;
