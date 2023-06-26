b638c056d7394273d517353671d8cf15c2483d87
Karim-Moharm <karimmoharm222@gmail.com>
2023-06-25 17:42:39 -0700

    pint fixed

--- opcode_1.c
+++ opcode_1.c
@@ -62,7 +62,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can\'t print an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -84,7 +84,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: can\'t pop an empty stack\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
